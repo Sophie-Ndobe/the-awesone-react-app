675c431943eaaa4866cd1772680c21d864e780a9
Sophie-Ndobe <ndobes76@gmail.com>
2023-10-05 19:11:34 +0200

    Updated the Search.js component and got the value of the search action

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Search.css";
 
 export default function Search() {
+    const [city, setCity] = useState(" ");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        alert(`${city}`);
+    }
+
+    function updateCity(event) {
+       setCity(event.target.value);
+    }
   return (
     <div className="Search">
-      <form>
-        <input type="Search" />
-        <button className="btn btn-primary mb-1">Search</button>
+      <form onSubmit={handleSubmit}>
+        <input type="Search" onChange={updateCity}/>
+        <button className="btn btn-primary mb-1" >Search</button>
         <button className="btn btn-success ms-1 mb-1">Current</button>
       </form>
     </div>
