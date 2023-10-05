114ac3d22c886bbd1e5658fc7017346b88c3dbab
Sophie-Ndobe <ndobes76@gmail.com>
2023-10-05 19:28:28 +0200

    Included an API call in the Search.js component

--- src/Search.js
+++ src/Search.js
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Search.css";
 
 export default function Search() {
-    const [city, setCity] = useState(" ");
+  const [city, setCity] = useState(" ");
+  const [weather, setWeather] = useState(" ");
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        alert(`${city}`);
+  function displayWeather(response) {
+    console.log(response);
+    setWeather({
+        city: response.data.city,
+        temp: response.data.temperature.current,
+        humidity: response.data.temperature.humidity,
+        wind: response.data.wind.speed,
     }
+    )
+  }
 
-    function updateCity(event) {
-       setCity(event.target.value);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
   return (
     <div className="Search">
       <form onSubmit={handleSubmit}>
-        <input type="Search" onChange={updateCity}/>
-        <button className="btn btn-primary mb-1" >Search</button>
+        <input type="Search" onChange={updateCity} />
+        <button className="btn btn-primary mb-1">Search</button>
         <button className="btn btn-success ms-1 mb-1">Current</button>
+      <h1>{weather.city}</h1>
       </form>
     </div>
   );
