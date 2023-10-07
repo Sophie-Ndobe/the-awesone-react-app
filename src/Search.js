2b70ad5f2115aaae4d13b5845e5321bfe8cc7e80
Sophie-Ndobe <ndobes76@gmail.com>
2023-10-08 00:08:45 +0200

    Created a WeatherForecast.js component and updated some forecast information

--- src/Search.js
+++ src/Search.js
@@ -3,10 +3,12 @@ import axios from "axios";
 import Weather from "./Weather";
 import "./Search.css";
 import CitiesPanel from "./CitiesPanel";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Search() {
   const [city, setCity] = useState("Tokyo");
   const [weather, setWeather] = useState(" ");
+  const [forecast, setForecast] = useState(" ");
 
   function displayWeather(response) {
     setWeather({
@@ -15,6 +17,30 @@ export default function Search() {
       humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
       description: response.data.condition.description,
+      lat: response.data.coordinates.latitude,
+      lon: response.data.coordinates.longitude,
+      time: response.data.time,
+    });
+  }
+
+  function displayWeatherForecast(response) {
+    console.log(response);
+    setForecast({
+      daily0: Math.round(response.data.daily[0].temperature.day),
+      icon0: response.data.daily[0].condition.icon_url,
+      time0: response.data.daily[0].time,
+      daily1: Math.round(response.data.daily[1].temperature.day),
+      icon1: response.data.daily[1].condition.icon_url,
+      time1: response.data.daily[1].time,
+      daily2: Math.round(response.data.daily[2].temperature.day),
+      icon2: response.data.daily[2].condition.icon_url,
+      time2: response.data.daily[2].time,
+      daily3: Math.round(response.data.daily[3].temperature.day),
+      icon3: response.data.daily[3].condition.icon_url,
+      time3: response.data.daily[3].time,
+      daily4: Math.round(response.data.daily[4].temperature.day),
+      icon4: response.data.daily[4].condition.icon_url,
+      time4: response.data.daily[4].time,
     });
   }
 
@@ -23,6 +49,10 @@ export default function Search() {
     let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
+
+    // let forecastApi = `https://api.shecodes.io/weather/v1/forecast?lon=${weather.lon}&lat=${weather.lat}&key=${apiKey}&units=metric`;
+    let forecastApi = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(forecastApi).then(displayWeatherForecast);
   }
 
   function updateCity(event) {
@@ -42,6 +72,23 @@ export default function Search() {
         humidity={weather.humidity}
         wind={weather.wind}
         descrip={weather.description}
+      />
+      <WeatherForecast
+        forecast0={forecast.daily0}
+        forecast1={forecast.daily1}
+        forecast2={forecast.daily2}
+        forecast3={forecast.daily3}
+        forecast4={forecast.daily4}
+        time0={forecast.time0}
+        time1={forecast.time1}
+        time2={forecast.time2}
+        time3={forecast.time3}
+        time4={forecast.time4}
+        icon0={forecast.icon0}
+        icon1={forecast.icon1}
+        icon2={forecast.icon2}
+        icon3={forecast.icon3}
+        icon4={forecast.icon4}
       />
     </div>
   );
---
+++ src/WeatherForecast.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function ForecastDate(props) {
+  let date0 = new Date(props.time0 * 1000);
+  let date1 = new Date(props.time1 * 1000);
+  let date2 = new Date(props.time2 * 1000);
+  let date3 = new Date(props.time3 * 1000);
+  let date4 = new Date(props.time4 * 1000);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let day0 = days[date0.getDay()];
+  let day1 = days[date1.getDay()];
+  let day2 = days[date2.getDay()];
+  let day3 = days[date3.getDay()];
+  let day4 = days[date4.getDay()];
+
+  let weatherForecast0 = props.forecast0;
+  let weatherForecast1 = props.forecast1;
+  let weatherForecast2 = props.forecast2;
+  let weatherForecast3 = props.forecast3;
+  let weatherForecast4 = props.forecast4;
+
+  
+
+  let conditions0 = [day0, weatherForecast0];
+  let conditions1 = [day1, weatherForecast1];
+  let conditions2 = [day2, weatherForecast2];
+  let conditions3 = [day3, weatherForecast3];
+  let conditions4 = [day4, weatherForecast4];
+
+  
+
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div>
+            {conditions0.map(function (condition, index) {
+              return <div key={index}>{condition}</div>;
+            })}
+          </div>
+        </div>
+        <div className="col">
+          <div>
+            {conditions1.map(function (condition, index) {
+              return <div key={index}>{condition}</div>;
+            })}
+          </div>
+        </div>
+        <div className="col">
+          <div>
+            {conditions2.map(function (condition, index) {
+              return <div key={index}>{condition}</div>;
+            })}
+          </div>
+        </div>
+        <div className="col">
+          <div>
+            {conditions3.map(function (condition, index) {
+              return <div key={index}>{condition}</div>;
+            })}
+          </div>
+        </div>
+        <div className="col">
+          <div>
+            {conditions4.map(function (condition, index) {
+              return <div key={index}>{condition}</div>;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
